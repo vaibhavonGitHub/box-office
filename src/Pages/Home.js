b6a285bcfc49c2861e2df2452bc2870bdc69fc3b
vaibhavonGitHub <69508218+vaibhavonGitHub@users.noreply.github.com>
2022-07-16 14:19:58 +0530

    added search bar and fetch results from api

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import MainPageLayout from "../Components/MainPageLayout";
+import React, { useState } from 'react';
+import MainPageLayout from '../Components/MainPageLayout';
 
 function Home() {
-  return <MainPageLayout>This is Home</MainPageLayout>;
-};
+  const [input, setInput] = useState('');
+
+  const onInput = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvamze.com/search/shows?q=${input}`)
+      .then(r => r.json)
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onKeyDown = (ev) =>{
+if(ev.keyCode === 13){
+  onSearch()
+}
+  };
+
+  return (
+    <MainPageLayout>
+      <input type="text" onChange={onInput} onKeyDown={onKeyDown} value={input} />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
+}
 
 export default Home;
