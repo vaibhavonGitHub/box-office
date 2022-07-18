dbe9b7190583fb56adeaacb4512865203fd3ff88
vaibhavonGitHub <69508218+vaibhavonGitHub@users.noreply.github.com>
2022-07-18 19:14:29 +0530

    using custom hooks for starred shows

--- src/Components/show/Showcard.js
+++ src/Components/show/Showcard.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-import {StyledShowCard} from './ShowCard.Styled';
+import { Link } from 'react-router-dom';
+import { Star } from '../Styled';
+import { StyledShowCard } from './ShowCard.Styled';
 
-function Showcard({ id, image, name, summary }) {
+function Showcard({ id, image, name, summary, onStarClick, isStarred }) {
   const summaryAsText = summary
-    ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, "")}...`
+    ? `${summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')}...`
     : 'No description';
 
   return (
     <StyledShowCard>
-      <div className='img-wrapper'>
+      <div className="img-wrapper">
         <img src={image} alt="show" />
       </div>
 
@@ -17,13 +18,14 @@ function Showcard({ id, image, name, summary }) {
 
       <p>{summaryAsText}</p>
 
-      <div className='btns'>
+      <div className="btns">
         <Link to={`/show/${id}`}>Read more</Link>
-        <button type="button">Star me</button>
+        <button type="button" onClick={onStarClick}>
+          <Star active={isStarred} />
+        </button>
       </div>
     </StyledShowCard>
   );
-};
-
+}
 
 export default Showcard;
--- src/Components/show/Showgrid.js
+++ src/Components/show/Showgrid.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import Notfound from '../../images/notfound.png';
 import Showcard from './Showcard';
-import {FlexGrid} from '../Styled';
+import { FlexGrid } from '../Styled';
+import { useShows } from '../../misc/Custom-Hooks';
 
 function Showgrid({ data }) {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <Showcard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : Notfound}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showId: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showId: show.id });
+          }
+        };
+
+        return (
+          <Showcard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : Notfound}
+            summary={show.summary}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
 }
